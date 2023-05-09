b62a8465ad37d7b4befcdf96181059e25bfd02d2
Sarah Ayele <114018578+SarahAbera@users.noreply.github.com>
2023-05-09 13:32:56 -0700

    Update Kth Smallest Element in a Sorted Matrix.py

--- Kth Smallest Element in a Sorted Matrix.py
+++ Kth Smallest Element in a Sorted Matrix.py
@@ -6,3 +6,20 @@ class Solution:
                 array.append(matrix[r][c])
         array.sort()
         return array[k-1]
+
+ 
+# heap solution
+    
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        array = []
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                array.append(matrix[r][c])
+        
+        heapify(array)
+        while k > 1:
+            heappop(array)
+            k -= 1
+
+        return heappop(array)
