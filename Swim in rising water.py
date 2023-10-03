78b5cabbef7decd1a6847fed3556d02ae4fc2758
Sarah Ayele <114018578+SarahAbera@users.noreply.github.com>
2023-10-03 16:38:38 +0300

    Update and rename Swim in raising water.py to Swim in rising water.py

--- Swim in rising water.py
+++ Swim in rising water.py
@@ -3,17 +3,20 @@ class Solution:
         n = len(grid)
         directions = [(0,1), (1,0), (-1,0), (0,-1)]
         inbound = lambda x, y : 0 <= x < n and 0 <= y < n
-        least_time = 0
+
         pq = [(grid[0][0],(0,0))]
         heapify(pq)
         visited = set()
+        least_time = 0
         while pq:
             time, cell = heappop(pq)
             r , c = cell
             visited.add(cell)
             least_time = max(least_time,time)
+
             if (r,c) == (n-1, n-1):
                 return least_time
+                
             for dx,dy in directions:
                 new_r = r + dx
                 new_c = c + dy
