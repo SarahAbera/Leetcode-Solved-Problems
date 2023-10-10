d469c45ebda8e4f33910711f085bbce54d45965f
Sarah Ayele <114018578+SarahAbera@users.noreply.github.com>
2023-10-10 11:45:56 +0300

    Update Two City Scheduling.py

--- Two City Scheduling.py
+++ Two City Scheduling.py
@@ -14,3 +14,23 @@ class Solution:
             j += 1
             i -= 1
         return totalTo_b
+
+# solution 2
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        greedy = []
+        for idx, cost in enumerate(costs):
+            diff = cost[1] - cost[0]
+            greedy.append((diff,idx))
+
+        greedy.sort()
+        min_cost = 0
+        for i,cost in enumerate(greedy):
+            diff, idx = cost
+            if i >= len(greedy) // 2:
+                min_cost += costs[idx][0]
+            else:
+                min_cost += costs[idx][1]
+        
+        return min_cost
+        
